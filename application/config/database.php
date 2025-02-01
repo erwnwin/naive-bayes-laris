26fe5db68761cfd8e282d0f48132de415ca84735
erwnwin <erwin18@mhs.akba.ac.id>
2025-02-01 18:08:28 +0800

    Database Commit

--- application/config/database.php
+++ application/config/database.php
@@ -73,36 +73,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'issue-shop',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
-
-// server DB
 // $db['default'] = array(
 // 	'dsn'	=> '',
 // 	'hostname' => 'localhost',
-// 	'username' => 'hvsteofj_root',
-// 	'password' => '@Erwin2024#',
-// 	'database' => 'hvsteofj_pariwisata_pangkep',
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'database' => 'issue-shop',
 // 	'dbdriver' => 'mysqli',
 // 	'dbprefix' => '',
 // 	'pconnect' => FALSE,
@@ -114,7 +90,31 @@ $db['default'] = array(
 // 	'swap_pre' => '',
 // 	'encrypt' => FALSE,
 // 	'compress' => FALSE,
-// 	'stricton' => TRUE,
+// 	'stricton' => FALSE,
 // 	'failover' => array(),
 // 	'save_queries' => TRUE
 // );
+
+
+// server DB
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'gevdwkug_root',
+	'password' => '@Erwin2025#',
+	'database' => 'gevdwkug_issue-shop',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => TRUE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
